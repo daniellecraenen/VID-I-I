69a3ea00f5c0292d4f7e2494cee5e5ba3bf966cd
daniellecraenen <105719243+daniellecraenen@users.noreply.github.com>
2022-05-26 19:23:15 +0200

    Update main.js

--- Workshop_files_begin/04Animation/scripts/main.js
+++ Workshop_files_begin/04Animation/scripts/main.js
@@ -2,23 +2,14 @@
 
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.05;
-var rotationSpeed2 = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	if(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	
 	console.log(myOtherBox.object3D.rotation.x);
 	console.log(myOtherBox.object3D.rotation.y);
-	} else {
-	myOtherBox.object3D.rotation.x += rotationSpeed2;
-	myOtherBox.object3D.rotation.y += rotationSpeed2;
-	
-	console.log(myOtherBox.object3D.rotation.x);
-	console.log(myOtherBox.object3D.rotation.y);
-	}
 }
 
 setInterval(spin, -16); //equivalent to 60 fps
